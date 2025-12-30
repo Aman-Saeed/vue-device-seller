0ccba448386b802c8a419453d062566a758c793b
Amanullah Saeed <saeedarab29@gmail.com>
2025-12-30 14:00:49 +0430

    handle jwt response in device

--- src/services/device.service.js
+++ src/services/device.service.js
@@ -1,16 +1,22 @@
 import { BASE_API_URL } from "../common/Constants";
 import axios from "axios";
 import { authHeader } from "./base.service";
+import { handleResponseWithLoginCheck } from "./base.service";
 
 const API_URL = BASE_API_URL + "/api/devices";
 
 class DeviceService {
   saveDevice(device) {
-    return axios.post(API_URL, device, { headers: authHeader() });
+    const req = axios.post(API_URL, device, { headers: authHeader() });
+    return handleResponseWithLoginCheck(req);
   }
 
   deleteDevice(device) {
-    return axios.delete(API_URL + "/" + device.id, { headers: authHeader() });
+    const req = axios.delete(API_URL + "/" + device.id, {
+      headers: authHeader(),
+    });
+
+    return handleResponseWithLoginCheck(req);
   }
 
   getAllDevices() {
